e4b3c240a67bdf8f84b7f50ac6633ea174f1ab5b
kmctwebdev3 <webdev3@kmct.org>
2026-06-13 11:05:55 +0530

    dashboard ui setting

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,32 +1,27 @@
-import { useState } from "react";
-import Sidebar from "../features/dashboard/components/Sidebar";
-import Navbar from "../features/dashboard/components/Navbar";
-import SuperAdminDashboard from "../features/dashboard/pages/SuperAdminDashboard";
-import Administrator from "../features/dashboard/components/Administrator";
-import Maintenance from "../features/dashboard/components/Maintainance";
-
 const DashboardLayout = () => {
     const [activePage, setActivePage] = useState("dashboard");
+
 
     const renderPage = () => {
         switch (activePage) {
             case "administrator":
                 return <Administrator />;
-
             case "maintenance":
                 return <Maintenance />;
-
             default:
                 return <SuperAdminDashboard />;
         }
     };
 
     return (
-        <div className="bg-gray-50 min-h-screen">
-            <Sidebar />
+        <div className="bg-[#F8F9FB] min-h-screen">
             <Navbar />
 
-            {/* Main Content */}
+            <Sidebar
+                activePage={activePage}
+                setActivePage={setActivePage}
+            />
+
             <main className="ml-64 mt-[82px] p-6">
                 {renderPage()}
             </main>
